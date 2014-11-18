fcdf86da1729b4f8e94751ab2d489d87141a4b81
sgomezag <sandra.ga86@gmail.com>
2014-11-18 15:12:37 +0100

    poll controller separated

---
+++ src/main/webapp/scripts/PollCtrl.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var isDefined = angular.isDefined;
+
+pollsApp
+.controller('pollCtrl', function ($scope, pollService, $location, $routeParams, $filter) {
+        var pollId = $routeParams.pollId;
+        $scope.pollUrl = 'http://localhost:8080/polls/#/poll/' + pollId + '/fill';
+
+        $scope.createPoll = function () {
+            pollService.createPoll($scope.poll).then(function (id) {
+                $location.path('poll/' + id + '/options');
+            });
+        };
+
+        $scope.init = function () {
+            if (isDefined(pollId)) {
+                pollService.getPollById(pollId).then(function (data) {
+                    $scope.poll = data;
+                    $scope.poll.options = ["", "", ""];
+                });
+            }
+        };
+
+        $scope.addOption = function () {
+            $scope.poll.options.push("");
+        };
+
+        $scope.removeOption = function (index) {
+            $scope.poll.options.splice(index, 1);
+        };
+
+        $scope.saveOptions = function () {
+
+            pollService.savePoll($scope.poll).then(function () {
+                $location.path('/poll/' + pollId + '/share');
+            });
+        };
+
+        $scope.finishPoll = function () {
+            $location.path('/');
+        };
+
+    });
+
