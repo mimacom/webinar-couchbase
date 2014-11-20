0c27347470c347aa5608013b28703fd418c1568b
sago <sandra.gomez@mimacom.com>
2014-11-20 18:57:27 +0100

    Couchbase properties extracted to a property file

--- src/main/java/com/mimacom/polls/config/AppConfig.java
+++ src/main/java/com/mimacom/polls/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.mimacom.polls.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
@@ -11,18 +12,27 @@ import java.util.List;
 @EnableCouchbaseRepositories("com.mimacom.polls.repository")
 public class AppConfig extends AbstractCouchbaseConfiguration {
 
+    @Value("${couchbase.host}")
+    String couchbaseHost;
+
+    @Value("${couchbase.bucket}")
+    String couchbaseBucket;
+
+    @Value("${couchbase.bucket.pass}")
+    String couchbaseBucketPass;
+
     @Override
     protected List<String> bootstrapHosts() {
-        return Collections.singletonList("127.0.0.1");
+        return Collections.singletonList(couchbaseHost);
     }
 
     @Override
     protected String getBucketName() {
-        return "polls";
+        return couchbaseBucket;
     }
 
     @Override
     protected String getBucketPassword() {
-        return "";
+        return couchbaseBucketPass;
     }
 }
